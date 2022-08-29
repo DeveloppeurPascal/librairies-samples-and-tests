5d890afbb59c4d07d9eeeb623b1ce2d92bd4134a
Patrick PREMARTIN <patrick@premartin.fr>
2022-08-29 18:57:11 +0200

    add sample FMX.TextImageframe/UseLibraryAsTFrame

---
+++ FMX.TextImageFrame/UseLibraryAsTFrame/UseLibraryAsTFrame.dpr
@@ -0,0 +1,17 @@
+program UseLibraryAsTFrame;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fUseLibraryAsTFrame in 'fUseLibraryAsTFrame.pas' {Form1},
+  Olf.FMX.TextImageFrame in '..\..\_librairies\Olf.FMX.TextImageFrame.pas' {OlfFMXTextImageFrame: TFrame},
+  udm_CharacterImages in '..\_CharacterImages\udm_CharacterImages.pas' {dm_CharacterImages: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(Tdm_CharacterImages, dm_CharacterImages);
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
